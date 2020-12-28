11d25fa1464639132b3f96e8a216565c84f9de62
mohamedashry7 <mohamedelashry7777@gmail.com>
2020-12-28 22:58:52 +0200

    Finishing changes

--- KB.pl
+++ KB.pl
@@ -1,4 +1,4 @@
-ethan_loc(0,0).
-members_loc([[1,1],[1,2]]).
-submarine(0,2).
-capacity(1).
+ethan_loc(0,3).
+members_loc([[1,0],[2,1]]).
+submarine(3,2).
+capacity(2).
--- MissionImpossible_29.pl
+++ MissionImpossible_29.pl
@@ -9,50 +9,86 @@ is_there_soldier(Ex,Ey,[[X,Y]|T],L):-
     is_there_soldier(Ex,Ey,T,L1),
     L = [[X,Y]|L1].
     
-helper_goal(Ex,Ey,[],Ex,Ey,C,C,S,S).
+helper_goal(Ex,Ey,[],C,S,S):-
+    % Goal state.
+    capacity(C),
+    submarine(Ex,Ey).
 
-helper_goal(Ex,Ey,L,Sx,Sy,C,Cp,S,G):-
+helper_goal(Ex,Ey,L,C,S,G):-
     % move up. 
     Ex > 0,
     Ex1 is Ex-1,
-    helper_goal(Ex1,Ey,L,Sx,Sy,C,Cp,result(up,S),G).
-helper_goal(Ex,Ey,L,Sx,Sy,C,Cp,S,G):-
+    helper_goal(Ex1,Ey,L,C,result(up,S),G).
+helper_goal(Ex,Ey,L,C,S,G):-
     % move down
     Ex < 3,
     Ex1 is Ex+1,
-    helper_goal(Ex1,Ey,L,Sx,Sy,C,Cp,result(down,S),G).
+    helper_goal(Ex1,Ey,L,C,result(down,S),G).
 
-helper_goal(Ex,Ey,L,Sx,Sy,C,Cp,S,G):-
+helper_goal(Ex,Ey,L,C,S,G):-
     % move left
     Ey > 0, 
     Ey1 is Ey-1 , 
-    helper_goal(Ex,Ey1,L,Sx,Sy,C,Cp,result(left,S),G).
+    helper_goal(Ex,Ey1,L,C,result(left,S),G).
 
-helper_goal(Ex,Ey,L,Sx,Sy,C,Cp,S,G):-
+helper_goal(Ex,Ey,L,C,S,G):-
     % move right
     Ey < 3, 
     Ey1 is Ey+1 , 
-    helper_goal(Ex,Ey1,L,Sx,Sy,C,Cp,result(right,S),G).
+    helper_goal(Ex,Ey1,L,C,result(right,S),G).
 
 
 
-helper_goal(Ex,Ey,L,Sx,Sy,C,Cp,S,G):-
+helper_goal(Ex,Ey,L,C,S,G):-
     % carry
     C>0,
     is_there_soldier(Ex,Ey,L,L1),
     C1 is C-1,
-    helper_goal(Ex,Ey,L1,Sx,Sy,C1,Cp,result(carry,S),G).
+    helper_goal(Ex,Ey,L1,C1,result(carry,S),G).
 
-helper_goal(Ex,Ey,L,Ex,Ey,C,Cp,S,G):-
+helper_goal(Ex,Ey,L,C,S,G):-
     % drop
+    capacity(Cp),
     C\=Cp,
-    helper_goal(Ex,Ey,L,Ex,Ey,Cp,Cp,result(drop,S),G).
+    submarine(Ex,Ey),
+    helper_goal(Ex,Ey,L,Cp,result(drop,S),G).
+
+
+
+call_with_incremental_goal(Ex,Ey,L,C,StartState,D,S):-
+    call_with_depth_limit(helper_goal(Ex,Ey,L,C,StartState,S),D,R),
+    R\==depth_limit_exceeded.
+
+call_with_incremental_goal(Ex,Ey,L,C,StartState,D,S):-
+    call_with_depth_limit(helper_goal(Ex,Ey,L,C,StartState,S),D,R),
+    R==depth_limit_exceeded,
+    D1 is D+2, 
+    call_with_incremental_goal(Ex,Ey,L,C,StartState,D1,S).
+
+has_var(S):-
+    var(S).
+
+has_var(S):-
+    S = result(_,S1),
+    has_var(S1).
+
 
 
 goal(S):-
+    % If S has a var.
     ethan_loc(Ex,Ey),
     members_loc(L),
-    submarine(Sx,Sy),
     capacity(C),
-    call_with_depth_limit(helper_goal(Ex,Ey,L,Sx,Sy,C,C,s0,S),8,R),
+    has_var(S),
+    call_with_incremental_goal(Ex,Ey,L,C,s0,1,S).
+
+
+goal(S):-
+    % If S has not a var
+    ethan_loc(Ex,Ey),
+    members_loc(L),
+    capacity(C),
+    \+has_var(S),
+    call_with_depth_limit(helper_goal(Ex,Ey,L,C,s0,S),20,R),
+    write(R),
     R\==depth_limit_exceeded.
